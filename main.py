15a4195c31b21b4e811d52d56e5a9ce8e0ac35b2
Prasanth P <prassu04u@gmail.com>
2025-01-11 15:41:22 +0800

    added front end URL

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://storied-tartufo-11d3ec.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
